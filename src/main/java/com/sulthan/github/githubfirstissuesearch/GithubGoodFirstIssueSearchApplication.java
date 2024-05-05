270b112197b1b367c14522219226d704a3ac9f5d
sulthan309 <k.mohammedsulthan@yahoo.com>
2024-05-05 21:13:40 +0530

    Initial Commit

---
+++ src/main/java/com/sulthan/github/githubfirstissuesearch/GithubGoodFirstIssueSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sulthan.github.githubfirstissuesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGoodFirstIssueSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubGoodFirstIssueSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulthan/github/githubfirstissuesearch/GithubGoodFirstIssueSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulthan.github.githubfirstissuesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGoodFirstIssueSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
